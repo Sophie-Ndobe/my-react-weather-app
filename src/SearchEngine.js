58a235ad4d780759776d130be062c9d953e76565
Sophie-Ndobe <ndobes76@gmail.com>
2023-09-26 13:47:43 +0200

    Added state management to get city value in the SearchEngine

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchEngine.css";
 
 export default function SearchEngine() {
+  const [city, setCity] = useState(" ");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(city);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
   return (
     <div className="SearchEngine">
-      <form>
-        <input type="search"></input>
+      <form onSubmit={handleSubmit}>
+        <input type="search" onChange={updateCity}></input>
         <button className="btn btn-primary mb-1">Search</button>
         <button className="btn btn-success ms-2 mb-1">Current</button>
       </form>
