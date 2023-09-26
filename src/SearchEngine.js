26591154c1180518778e0fbba9190c2900aea104
Sophie-Ndobe <ndobes76@gmail.com>
2023-09-26 13:39:22 +0200

    Created a form in the SearchEngine component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "./SearchEngine.css";
 
 export default function SearchEngine() {
-    return <div className="SearchEngine">
-        The Search engine goes here!!!
+  return (
+    <div className="SearchEngine">
+      <form>
+        <input type="search"></input>
+        <button className="btn btn-primary mb-1">Search</button>
+        <button className="btn btn-success ms-2 mb-1">Current</button>
+      </form>
     </div>
+  );
 }
