c270768d946d4e6a3179ca8474ec07efeec079c2
Sophie-Ndobe <ndobes76@gmail.com>
2023-09-26 22:56:49 +0200

    Added the api call in the SearchEngine component

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./SearchEngine.css";
 import WeatherCondition from "./WeatherCondition";
 
 export default function SearchEngine() {
   const [city, setCity] = useState(" ");
+  const [weather, setWeather] = useState(" ");
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.temerature.current,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+    });
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
     alert(city);
+
+    let apiKey = "2c13e0a2b6fe347b0421bb02eef2o43t";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
 
   function updateCity(event) {
@@ -20,7 +35,7 @@ export default function SearchEngine() {
         <button className="btn btn-primary mb-1">Search</button>
         <button className="btn btn-success ms-2 mb-1">Current</button>
       </form>
-      <WeatherCondition />
+      <WeatherCondition temp={weather.temperature} />
     </div>
   );
 }
